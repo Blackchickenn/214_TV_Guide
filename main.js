900b002afb828c9298ca9266abe63a8b7b382aa4
Blackchickenn <marcel.madaj@gmail.com>
2022-03-04 11:50:50 +0100

    refactoring of .js file

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 async function findShow(query){
-    let response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons`)
+    let response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
     let data = await response.json()
     // console.log(data)
     return data
 }
 
+function getShowHtml(show){
+     return `<div class="my-show">
+        <div class="my-show-title">
+         ${show.name}
+        </div>
+        <div class="my-show-summary">
+         ${show.summary}
+        </div>
+    </div>`
+    
+}
 
+function displayShows(shows){
+    
+    document.body.innerHTML = `<div class="my-shows">
+    ${shows.map(show => getShowHtml(show.show)).join('')}
+    </div>`
+}
 
-findShow("office").then(show =>{
-    console.log(show)
-    document.body.innerHTML = `
-    <div class="my-show">
-        <div class="my-show-title">${show.name}</div>
-        <div class="my-show-summary">${show.summary}</div>
-        ${show._embedded.seasons.map(season =>{
-            return `<div class="my-show-season">Season ${season.number}</div>`
-        }).join('')}
-    </div>
-    `
-})
+findShow("friends").then(displayShows)
+.catch (error => console.log(error))
